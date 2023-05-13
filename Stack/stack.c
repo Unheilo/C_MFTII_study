1b67c038f14a542726871a0178d84d2a69a72416
Unheilo <131784536+Unheilo@users.noreply.github.com>
2023-05-14 00:07:44 +1000

    stack init

---
+++ Stack/stack.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define N 8
+
+typedef int Data;
+typedef struct {
+    Data a[N]; // данные
+    unsigned int n; // сколько элементов хранится в стеке
+};
+
+int main()
+{
+    Stack s = {{7, 4, 1}, 3};
+    Stack * st = &s;
+}
+
+//typedef int Data;
+//typedef struct {
+//    Data a[N]; // храним данные в системе
+//    int n; // сколько данных в стеке
+//} Stack;
+//
+//Stack st;   push(&st, 5)
+//
+//void push(Stack * s, Data x);
+//Data pop(Stack *s); // top
+//void init(Stack *s);
+//void print(Stack *s);
+//int is_empty(Stack *s); // int is_full(Stack *s)
