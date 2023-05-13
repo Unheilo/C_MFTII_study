f092d24f7fc15a0ab80dd941cbbc19c3f830009a
Unheilo <131784536+Unheilo@users.noreply.github.com>
2023-05-14 00:19:22 +1000

    print function

--- Stack/stack.c
+++ Stack/stack.c
@@ -7,10 +7,21 @@ typedef struct {
     unsigned int n; // сколько элементов хранится в стеке
 } Stack;
 
+void print(Stack * st)
+{
+    for (unsigned int i = 0; i < st->n; i++)
+        printf("%d ", st->a[i]);
+    printf("\n");
+}
+
 int main()
 {
     Stack s = {{7, 4, 1}, 3};
-    Stack * st = &s;
+    Stack *st = &s;
+
+    print(st);
+
+    return 0;
 }
 
 //typedef int Data;
