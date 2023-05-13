b45616b1fdecd6b60b26304d2a8b0a788ee1885c
Unheilo <Mincalm@gmail.com>
2023-05-14 04:59:37 +1000

    test refactoring

--- Stack/stack.c
+++ Stack/stack.c
@@ -7,78 +7,72 @@ typedef struct {
     unsigned int n; // сколько элементов хранится в стеке
 } Stack;
 
-void print(Stack * st)
+void print(Stack* st)
 {
     for (unsigned int i = 0; i < st->n; i++)
         printf("%d ", st->a[i]);
     printf("\n");
 }
 
-void init(Stack * st) {
+void init(Stack* st) {
     st->n = 0;
 }
 
-void push(Stack * st, Data data)
+void push(Stack* st, Data data)
 {
     st->a[st->n] = data;
-    st->n ++;
+    st->n++;
 }
 
-Data pop(Stack * st)
+Data pop(Stack* st)
 {
     Data res = st->a[st->n - 1];
-    st->n --;
+    st->n--;
     return res;
 }
 
-int is_empty(Stack * st) {
+int is_empty(Stack* st) {
     return st->n == 0;
 }
 
-int is_full(Stack * st) {
+int is_full(Stack* st) {
     return st->n == sizeof(st->a) / sizeof(st->a[0]);
 }
 
 int main()
 {
+    Data test[N] = { 5, 17, -3, 0, 1, 2, 3, 4 };
+
+
     Stack stack;            // создаем стек
-    Stack * st = &stack;    // указатель на созданный стек
+    Stack* st = &stack;    // указатель на созданный стек
 
     init(st);
     printf("empty: %s\n", is_empty(st) ? "YES" : "NO"); // YES
     printf("full: %s\n", is_full(st) ? "YES" : "NO");   // NO
     print(st);              // ничего не печатается
 
-    // тесты для push
-    push(st, 5);
-    print(st);      // 5
-    push(st, 17);
-    print(st);      // 5 17
-    push(st, -3);
-    print(st);      // 5 17 -3
+    Data d;
+    for (int i = 0; i < N; i++) {
+        d = test[i];
+        printf("push %d :", d);
+        push(st, d);
+        print(st);  // 5
+        printf("empty: %s\n", is_empty(st) ? "YES" : "NO"); // NO
+    }
 
-    printf("empty: %s\n", is_empty(st) ? "YES" : "NO"); // NO
     printf("full: %s\n", is_full(st) ? "YES" : "NO");   // YES
 
-    // тесты для pop
-    Data d;
-    d = pop(st);    // pop -3: 5 17
-    printf("pop %d: ", d);
-    print(st);
-
-    d = pop(st);    // pop 17: 5
-    printf("pop %d: ", d);
-    print(st);
-
-    d = pop(st);    // pop 5:
-    printf("pop %d: ", d);
-    print(st);
+    while (!is_empty(st)) {
+        d = pop(st);
+        printf("pop %d: ", d);
+        print(st);
+    }
 
     printf("empty: %s\n", is_empty(st) ? "YES" : "NO"); // YES
-    printf("full: %s\n", is_full(st) ? "YES" : "NO");   // NO
 
     return 0;
-    
+
 }
 
 //typedef int Data;
