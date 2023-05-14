11e2ebcf686fa271386ff5955eae1c4045f7a36c
Unheilo <Mincalm@gmail.com>
2023-05-15 03:17:28 +1000

    make tests

--- list/list1.c
+++ list/list1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef int Data;
 
@@ -8,6 +9,9 @@ struct Node {
 	Node* next;
 };
 
+void print(Node* list);
+void push(Node ** plist, Data d);
+
 void print(Node* list) {
 	for (Node* p = list; p != NULL; p = p->next) {
 		printf("%d ", p->data);
@@ -15,15 +19,21 @@ void print(Node* list) {
 	printf("\n");
 }
 
-int main() {
-	struct Node* list = NULL;
-	Node a = { 3 }, b = { 17 }, c = { 21 }, t = { 10 };
-	list = &a;
-	a.next = &b;
-	b.next = &c;
-	c.next = NULL;
+void push(Node** plist, Data d) {
+	Node* p = malloc(sizeof(Node));
+	p->data = d;
+	p->next = *plist;
+	*plist = p;
+}
 
-	print(list);
+int main() {
+	Data test[] = { 21, 17, 3, 10 };
+	Node* list = NULL;
+
+	for (size_t i = 0; i < sizeof(test) / sizeof(test[0]); i++) {
+		pust(&list, test[i]);
+		print(list);
+	}
 
 	return 0;
 }
