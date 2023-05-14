14d200a212e8fe295e8a0a73060e579c961baf51
Unheilo <Mincalm@gmail.com>
2023-05-15 03:37:37 +1000

    add pop function and tests

--- list/list1.c
+++ list/list1.c
@@ -12,6 +12,7 @@ struct Node {
 void print(Node* list);
 int is_empty(Node* list);
 void push(Node ** plist, Data d);
+Data pop(Node** plist);
 
 void print(Node* list) {
 	for (Node* p = list; p != NULL; p = p->next) {
@@ -25,6 +26,14 @@ void push(Node** plist, Data d) {
 	p->data = d;
 	p->next = *plist;
 	*plist = p;
+}
+
+Data pop(Node** plist) {
+	Node* p = *plist;
+	Data res = p->data;
+	*plist = p->next;
+	free(p);
+	return res;
 }
 
 int is_empty(Node* list) {
@@ -41,5 +50,13 @@ int main() {
 	}
 	printf("Empty: %s\n", is_empty(list) ? "YES" : "NO");
 
+	while (!is_empty(list)) {
+		Data d = pop(&list);
+		printf("pop %d :", d);
+		print(list);
+	}
+	printf("Empty: %s\n", is_empty(list) ? "YES" : "NO");
+
 	return 0;
 }
+
