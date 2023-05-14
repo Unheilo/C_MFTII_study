fc1b2a53bdf05e8fd119f3867f719c9f625afeb2
Unheilo <Mincalm@gmail.com>
2023-05-15 01:52:23 +1000

    list init model 2

--- list/list1.c
+++ list/list1.c
@@ -3,7 +3,6 @@
 typedef int Data;
 
 typedef struct Node Node;
-
 struct Node {
 	Data data;
 	Node* next;
@@ -11,5 +10,10 @@ struct Node {
 
 int main() {
 	struct Node* list = NULL;
+	Node a = { 3 }, b = { 17 }, c = { 21 }, t = { 10 };
+	list = &a;
+	a.next = &b;
+	b.next = &c;
+	c.next = NULL;
 	return 0;
 }
