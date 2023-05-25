3e192d02617ea01a183146e87b9e936d2e0f1711
Unheilo <Mincalm@gmail.com>
2023-05-25 13:11:47 +1000

    prepare to lab 2

--- out/build/x64-Debug/cmake_install.cmake
+++ out/build/x64-Debug/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Users/Admin/Documents/github/out/install/x64-Debug")
+  set(CMAKE_INSTALL_PREFIX "C:/Users/Admin/Documents/GitHub/out/install/x64-Debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
